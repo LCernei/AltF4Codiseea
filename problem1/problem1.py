e8fdcbe164e314983c59b7a24d4aeb7206256424
Liviu Cernei <cernei.liviu@gmail.com>
2020-12-05 21:50:55 +0200

    Update problem 1

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -1,7 +1,4 @@
 from PIL import Image
-# 59008105 incorrect: sum red channel values for all pixels
-# 09820390 incorrect: sum red channel values for non white pixels
-# 49187715 incorrect
 
 im = Image.open('best.bmp')
 sumAll = 0
@@ -9,5 +6,4 @@ for x in range(im.width):
     for y in range(im.height):
         r, g, b = im.getpixel((x, y))
         sumAll += r
-        count += 1
-print(sumAll)
+print(sumAll)  # 59008105
